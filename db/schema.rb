58d02e993d6f099acff19ae1a6d5885e638b2454
JuanCabelloVargas <jicabello@miuandes.cl>
2025-09-04 17:46:31 -0400

    feat: Create Guests table with first_name, last_name, email, phone and document_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_04_212511) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_04_214231) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "guests", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.string "phone", null: false
+    t.string "document_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "number", null: false
