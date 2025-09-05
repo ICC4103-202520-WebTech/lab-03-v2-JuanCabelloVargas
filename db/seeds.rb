2c730b4348d99031eb7c7949c3688c891a9f4479
JuanCabelloVargas <jicabello@miuandes.cl>
2025-09-05 13:52:58 -0400

    feat: Create Guest and Room data in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Initializing data loading process"
+
+puts "Loading guests in table"
+
+Guest.create!([
+  { first_name: "Andres", last_name: "Howard", email: "aihoward@miuandes.cl", phone: "900001111", document_id: "99999999k" },
+  { first_name: "Elvis", last_name: "Tec", email: "test@test.com", phone: "912345678", document_id: "141414142" }
+])
+
+Room.create!([
+  { number: "101", room_type: 1, price: 1000, status: 1 },
+  { number: "201", room_type: 2, price: 2000, status: 2 },
+  { number: "301", room_type: 3, price: 3000, status: 3 }
+])
+
+
+
