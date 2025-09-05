3150219ff3a547cc88484f5ddc814ebcaf722f3c
JuanCabelloVargas <jicabello@miuandes.cl>
2025-09-05 13:14:02 -0400

    feat: Create Services table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_05_165941) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_05_170817) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -42,6 +42,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_05_165941) do
     t.integer "room_type", null: false
     t.integer "price", null: false
     t.integer "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.boolean "is_active", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
