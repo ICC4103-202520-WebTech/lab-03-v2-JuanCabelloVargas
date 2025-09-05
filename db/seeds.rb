70061d21b124b2678487b363385a6fcaecc4a8f8
JuanCabelloVargas <jicabello@miuandes.cl>
2025-09-05 14:24:10 -0400

    feat: Create initial data of reservation on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,4 +22,13 @@ Service.create!([
   { name: "Spa", price: 15, is_active: false }
 ])
 
+puts "Loading Reservations"
+
+Reservation.create!([
+  { code: "code1", guest_id: 1, room_id: 1, check_in: Date.new(2025, 9, 1), check_out: Date.new(2025, 9, 15), status: 1, adults: 1, children: 2 },
+  { code: "code2", guest_id: 2, room_id: 3, check_in: Date.new(1995, 12, 21), check_out: Date.new(9999, 12, 31), status: 1, adults:1 }
+])
+
+
+
 
